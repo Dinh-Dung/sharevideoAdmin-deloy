dc2b4f41f00ae7bee0e8991ffb331baadf83d5de
Dung-Dinh <dungvanbmt.01@gmail.com>
2023-05-23 19:22:15 +0700

    fix 4

--- src/components/Navbars/AuthNavbar.tsx
+++ src/components/Navbars/AuthNavbar.tsx
@@ -86,7 +86,7 @@ const AdminNavbar = () => {
                   <span className="nav-link-inner--text">Login</span>
                 </NavLink>
               </NavItem> */}
-              <NavItem>
+              {/* <NavItem>
                 <NavLink
                   className="nav-link-icon"
                   to="/admin/user-profile"
@@ -95,7 +95,7 @@ const AdminNavbar = () => {
                   <i className="ni ni-single-02" />
                   <span className="nav-link-inner--text">Profile</span>
                 </NavLink>
-              </NavItem>
+              </NavItem> */}
             </Nav>
           </UncontrolledCollapse>
         </Container>
